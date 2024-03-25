88303c9821ddf7fcf3cb6e2c16645ab7f6d70745
RobsonMendes37 <robsonqueirozmendes@alu.ufc.br>
2024-03-24 21:17:14 -0300

    Resposta da Atividade02 de Web

--- repositorio/src/App.js
+++ repositorio/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { PlacaMae as PCPlacaMae, Memoria as PCMemoria, PlacaDeVideo as PCPlacaDeVideo } from './components/atividade01/02MeuPC';
+import { Hero as BatalhaHero, Enemy as BatalhaEnemy, Arena as BatalhaArena, World as BatalhaWorld } from './components/atividade01/03Batalha';
+
+// Importando as imagens
+import heroImage from './components/atividade01/assets/hero.jpg';
+import enemyImage from './components/atividade01/assets/enemy.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Componentes da segunda questão */}
+      <PCPlacaMae nome="Placa-mãe XYZ" preco="500.00" />
+      <PCMemoria nome="Memória DDR4" preco="200.00" />
+      <PCPlacaDeVideo nome="Placa de Vídeo GTX 1080" preco="800.00" />
+
+      {/* Componentes da terceira questão */}
+      <BatalhaWorld>
+        <BatalhaArena>
+          {/* Passando as imagens como props */}
+          <BatalhaHero name="supermen" img={heroImage} />
+          <BatalhaEnemy name="tictoker" img={enemyImage} />
+        </BatalhaArena>
+        <BatalhaArena>
+          <BatalhaHero name="supermen_Alternativo" img={heroImage} />
+          <BatalhaEnemy name="tictoker de Tal" img={enemyImage} />
+        </BatalhaArena>
+      </BatalhaWorld>
     </div>
   );
 }
