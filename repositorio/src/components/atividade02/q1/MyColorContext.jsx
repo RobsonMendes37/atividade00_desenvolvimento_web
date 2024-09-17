b19ac3fd529b63cf4bdbafa8dd42aaba49c18cbc
Francisco Robson Queiroz Mendes <robsonqueirozmendes@alu.ufc.br>
2024-09-17 13:21:54 -0300

    Delete repositorio directory

--- repositorio/src/components/atividade02/q1/MyColorContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const cores = { bkgA: "green", bkgB: "white", bkgC: "yellow", bkgD: "blue" };
-const ColorTheme = createContext(cores);
-
-export default ColorTheme;
